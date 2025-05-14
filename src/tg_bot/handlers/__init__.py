1e430d95dc5300c7362a8cf3350c9ebe762c7ec0
h0guera <shalyginetc@gmail.com>
2025-05-14 21:11:03 +0300

    handlers setups with dialogs

--- src/tg_bot/handlers/__init__.py
+++ src/tg_bot/handlers/__init__.py
@@ -1,12 +1,16 @@
 import logging
 
 from aiogram import Dispatcher
+from aiogram_dialog.api.protocols import MessageManagerProtocol
 
+from tg_bot import dialogs
 from tg_bot.handlers import base
 
 logger = logging.getLogger(__name__)
 
 
-def setup_handlers(dp: Dispatcher) -> None:
+def setup_handlers(dp: Dispatcher, message_manager: MessageManagerProtocol) -> None:
+    dialogs.setup(dp, message_manager)
+
     dp.include_router(base.setup())
     logger.debug("handlers configured successfully")
